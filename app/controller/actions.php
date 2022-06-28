4badbe96c0dc315b8bfd0f0ba630f1736282f99e
v-verner <vini.verner1@gmail.com>
2022-06-28 17:59:27 -0300

    Analytics

--- app/controller/actions.php
+++ app/controller/actions.php
@@ -8,3 +8,37 @@ function insertFooterNote(): void
 {
     require_once FULL_CUSTOMER_APP . '/views/footer/note.php';
 }
+
+function activationHook(): void
+{
+    $url = defined('FULL_CUSTOMER_ENV') && FULL_CUSTOMER_ENV === 'DEV' ?
+            'https://full.dev' :
+            'https://painel.fullstackagency.club';
+
+    wp_remote_post($url . '/wp-json/full-customer/v1/analytics', [
+        'sslverify' => false,
+        'headers'   => ['x-full' => 'Jkd0JeCPm8Nx', 'Content-Type' => 'application/json'],
+        'body'      => json_encode([
+            'site_url'      => home_url(),
+            'admin_email'   => get_bloginfo('admin_email'),
+            'plugin_status' => 'active'
+        ])
+    ]);
+}
+
+function deactivationHook(): void
+{
+    $url = defined('FULL_CUSTOMER_ENV') && FULL_CUSTOMER_ENV === 'DEV' ?
+            'https://full.dev' :
+            'https://painel.fullstackagency.club';
+
+    wp_remote_post($url . '/wp-json/full-customer/v1/analytics', [
+        'sslverify' => false,
+        'headers'   => ['x-full' => 'Jkd0JeCPm8Nx', 'Content-Type' => 'application/json'],
+        'body'      => json_encode([
+            'site_url'      => home_url(),
+            'admin_email'   => get_bloginfo('admin_email'),
+            'plugin_status' => 'inactive'
+        ])
+    ]);
+}
--- app/controller/hooks.php
+++ app/controller/hooks.php
@@ -8,3 +8,6 @@ add_action('rest_api_init', ['\FULL_CUSTOMER_Login', 'registerRoutes']);
 add_action('rest_api_init', ['\FULL_CUSTOMER_Plugin', 'registerRoutes']);
 
 add_action('wp_footer', '\Full\Customer\Actions\insertFooterNote');
+
+register_activation_hook( FULL_CUSTOMER_FILE, '\Full\Customer\Actions\activationHook' );
+register_deactivation_hook( FULL_CUSTOMER_FILE, '\Full\Customer\Actions\deactivationHook' );
