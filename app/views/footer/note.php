bab56fee42522e8db2451c92323b81880531f7ff
v-verner <vini.verner1@gmail.com>
2022-07-21 16:25:06 -0300

    ajuste na visibilidade da copy

--- app/views/footer/note.php
+++ app/views/footer/note.php
@@ -1,1 +1,1 @@
-<a href="https://fullstackagency.club/" style="visibility: hidden; width: 0; height: 0; user-select: none; pointer-events: none; display: block;">plugins premium WordPress</a>
+<a href="https://fullstackagency.club/" style="visibility: hidden; user-select: none; pointer-events: none; display: none;">plugins premium WordPress</a>
