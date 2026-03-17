d5c120dc8a6704703903fd054621e1806638a7fe
v-verner <vini.verner1@gmail.com>
2026-03-17 09:19:53 -0300

    requires antigos

--- app/init.php
+++ app/init.php
@@ -9,18 +9,15 @@ require_once FULL_CUSTOMER_APP . '/controller/inc/License.php';
 require_once FULL_CUSTOMER_APP . '/api/Controller.php';
 
 require_once FULL_CUSTOMER_APP . '/api/Connection.php';
-require_once FULL_CUSTOMER_APP . '/api/Copy.php';
 require_once FULL_CUSTOMER_APP . '/api/Env.php';
 require_once FULL_CUSTOMER_APP . '/api/Health.php';
 require_once FULL_CUSTOMER_APP . '/api/PluginInstallation.php';
 require_once FULL_CUSTOMER_APP . '/api/ElementorTemplates.php';
-require_once FULL_CUSTOMER_APP . '/api/Widgets.php';
 
 require_once FULL_CUSTOMER_APP . '/controller/inc/Health.php';
 require_once FULL_CUSTOMER_APP . '/controller/inc/RemoteLogin.php';
 
 if (License::isActive()) :
-  require_once FULL_CUSTOMER_APP . '/api/PluginUpdate.php';
   require_once FULL_CUSTOMER_APP . '/api/Whitelabel.php';
 endif;
 
