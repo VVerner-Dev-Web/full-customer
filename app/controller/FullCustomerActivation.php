cd79a75075b9fe952f661e99bfe50e0993ff450c
v-verner <vini.verner1@gmail.com>
2026-03-20 17:33:00 -0300

    desconsiderar addoins

--- app/controller/FullCustomerActivation.php
+++ app/controller/FullCustomerActivation.php
@@ -62,8 +62,7 @@ class FullCustomerActivation
     }
 
     $dir = array_map([$this, 'localPluginInfo'], FullCustomerUpdate::fetchDirectory(false));
-
-    $dir = array_filter($dir, fn($plugin) => !str_contains($plugin->plugin, 'full-customer'));
+    $dir = array_filter($dir, fn($plugin) => !str_contains($plugin->plugin, 'full-customer') && !$plugin->is_addon);
 
     wp_send_json_success(array_values($dir));
   }
