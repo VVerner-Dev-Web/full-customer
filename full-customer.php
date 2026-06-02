092699c27cce981b3c812dc91813ce0f7875bb09
v-verner <vini.verner1@gmail.com>
2026-06-02 15:58:36 -0300

    atualizado versão para 4.0.1

--- full-customer.php
+++ full-customer.php
@@ -7,9 +7,9 @@ defined('ABSPATH') || exit;
 /**
  * Plugin Name:         FULL.Cliente
  * Description:         Este plugin adiciona novas extensões úteis e conecta-o ao painel da FULL. para ativações de outros plugins.
- * Version:             4.0.0
+ * Version:             4.0.1
  * Requires at least:   6.5
- * Tested up to:        6.9
+ * Tested up to:        7.0
  * Requires PHP:        7.4
  * Author:              FULL.
  * Author URI:          https://full.services/
@@ -28,7 +28,7 @@ if (!defined('FULL_CUSTOMER_VERSION')) {
     define('FULL_CUSTOMER_DEV', strpos(home_url(), '.dev') !== false);
   }
 
-  define('FULL_CUSTOMER_VERSION', FULL_CUSTOMER_DEV ? uniqid() : '4.0.0');
+  define('FULL_CUSTOMER_VERSION', FULL_CUSTOMER_DEV ? uniqid() : '4.0.1');
   define('FULL_CUSTOMER_API_DOMAIN', FULL_CUSTOMER_DEV ? 'https://full.dev/' : 'https://api.full.services/');
   define('FULL_CUSTOMER_API_URL', FULL_CUSTOMER_DEV ? FULL_CUSTOMER_API_DOMAIN . 'wp-json/fc-ai' : FULL_CUSTOMER_API_DOMAIN . 'wp-json/fc-ai');
 
