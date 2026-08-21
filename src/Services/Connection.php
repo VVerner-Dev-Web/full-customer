479099c0f00182e3f2ae0908e7c3c15e2db07719
v-verner <vini.verner1@gmail.com>
2026-08-21 16:35:15 -0300

    Novos parametros da assinatura

--- src/Services/Connection.php
+++ src/Services/Connection.php
@@ -96,6 +96,8 @@ class Connection
       'connection_email' => is_user_logged_in() ? $user->getConnectionEmail() : ($anon ? $anon['connection_email'] : null),
       'wp_user_email' => is_user_logged_in() ? $user->wp()->user_email : ($anon ? get_userdata($anon['user_id'])->user_email : null),
       'wp_site_url' => trailingslashit(home_url()),
+      'wp_admin_url' => trailingslashit(admin_url()),
+      'wp_login_url' => trailingslashit(login_url()),
     ];
 
     return base64_encode(wp_json_encode(apply_filters('fc/connection/token', $params)));
